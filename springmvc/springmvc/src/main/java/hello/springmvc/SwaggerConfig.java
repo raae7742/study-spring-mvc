e14292f067e416e890c463d0d4089e0789c16c7c
Hyeonae Jang <aeae_1@naver.com>
2021-11-12 00:58:31 +0900

    [Section 6-2] Spring MVC - 기본 기능 (header, param, request, response)

---
+++ springmvc/springmvc/src/main/java/hello/springmvc/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package hello.springmvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("hello.springmvc.basic.requestmapping"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger 연습")
+                .description("2021-11-08 swagger 연습")
+                .version("1.0")
+                .build();
+    }
+}
--- springmvc/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -1,11 +1,23 @@
 package hello.springmvc.basic.requestmapping;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/mapping/users")
 public class MappingClassController {
 
+    @Operation(summary = "회원 조회", description = "회원 정보를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping
     public String user() {
         return "get users";
@@ -16,6 +28,13 @@ public class MappingClassController {
         return "post user";
     }
 
+    @ApiImplicitParam(
+            name = "userId",
+            value = "회원 아이디",
+            required = true,
+            dataType = "string",
+            paramType = "path"
+    )
     @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
         return "get userId=" + userId;
